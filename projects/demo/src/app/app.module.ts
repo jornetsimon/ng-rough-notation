a97b4161f845188ac4e79fa53e3354009541c3ff
Simon Jornet <jornetsimon@gmail.com>
2020-06-12 16:04:27 +0200

    Demo: show input

--- projects/demo/src/app/app.module.ts
+++ projects/demo/src/app/app.module.ts
@@ -3,10 +3,11 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { RoughNotationModule } from '../../../ng-rough-notation/src/lib/rough-notation.module';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
 	declarations: [AppComponent],
-	imports: [BrowserModule, RoughNotationModule],
+	imports: [BrowserModule, RoughNotationModule, FormsModule],
 	providers: [],
 	bootstrap: [AppComponent],
 })
