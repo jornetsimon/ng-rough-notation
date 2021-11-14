9caf3a355d5c25c880476ea14e9b9ea23d37a6f5
Simon Jornet <jornetsimon@gmail.com>
2021-11-14 07:38:31 +0100

    Angular 13

--- projects/demo/src/test.ts
+++ projects/demo/src/test.ts
@@ -3,8 +3,8 @@
 import 'zone.js/testing';
 import { getTestBed } from '@angular/core/testing';
 import {
-  BrowserDynamicTestingModule,
-  platformBrowserDynamicTesting,
+	BrowserDynamicTestingModule,
+	platformBrowserDynamicTesting,
 } from '@angular/platform-browser-dynamic/testing';
 
 declare const require: {
@@ -17,7 +17,9 @@ declare const require: {
 // First, initialize the Angular testing environment.
 getTestBed().initTestEnvironment(
   BrowserDynamicTestingModule,
-  platformBrowserDynamicTesting()
+  platformBrowserDynamicTesting(), {
+    teardown: { destroyAfterEach: false }
+}
 );
 // Then we find all the tests.
 const context = require.context('./', true, /\.spec\.ts$/);
